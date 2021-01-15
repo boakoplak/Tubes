0c18535b0583e18cf9f67365a2ff56972e80b80d
boakoplak <rahmatnurdinnugraha@gmail.com>
2021-01-15 13:05:41 +0700

    PacGame

---
+++ Assets/Script/playerControler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            rb2d.velocity = Vector2.left * speed;
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            rb2d.velocity = Vector2.right * speed;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            rb2d.velocity = Vector2.down * speed;
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            rb2d.velocity = Vector2.up * speed;
+        } 
+    }
+}
